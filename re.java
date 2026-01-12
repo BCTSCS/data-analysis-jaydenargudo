05d7bd56603a86ab8c4b8d88f0682588f1a85000
Jayden Argudo <jayarg26@bergen.org>
2026-01-12 14:05:51 -0500

    1/12

--- re.java
+++ re.java
@@ -9,15 +9,38 @@ public class re {
         //read posts.txt
         //Step 1 FileOperator obj
         ArrayList<String> posts = FileOperator.getStringList("posts.txt");
-        System.out.println(posts);
+        // System.out.println(posts);
 
         //Iterate each post and fine the #\w
-        for (String post : posts) {
-            boolean result = post.matches(re);
+        // for (String post : posts) {
+        //     boolean result = post.matches(re);
+        //     System.out.println(post + " : " + result);
+        // }
+
+
+        //Check zipcodes to see if they match the pattern
+        // String zipPattern = "^\\d{5}(-\\d{4})?$";
+        // String[] zipcodes = {"12345-6789", "98765-4321", "8989"};
+
+        // for (String zip : zipcodes){
+        //     boolean result = zip.matches(zipPattern);
+        //     System.out.println(zip + " is " + result);
+        // }
+
+        //Find all words that start with a capital letter
+        String[] words = {"Apple", "banana", "Orange", "grape", "Pineapple"};
+        String capitalWords = "\\b[A-Z][a-zA-Z]*\\b";
+
+        for (String word : words) {
+            boolean result = word.matches(capitalWords);
+            if (result == true) {
+                System.out.println(word + " is a capitalized word.");
+            }
         }
 
-        boolean result = t.matches(re);
 
-        System.out.println(result);
+        // boolean result = t.matches(re);
+
+        // System.out.println(result);
     }
 }
